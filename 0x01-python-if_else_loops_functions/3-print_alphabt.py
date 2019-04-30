618cf3fb97864f6f63f40a647c8deb8c5d026e02
Sofia Cheung <659@holbertonschool.com>
2019-04-30 22:13:28 +0000

    changed print format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for letter in range(97, 123):
     if letter == 113 or letter == 101:
         continue
     else:
-        print(chr(letter), end="")
+        print("{:s}".format(chr(letter)), end="")
