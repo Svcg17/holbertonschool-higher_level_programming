605828998ce710579ba10c4d781c50acaae475b5
Sofia Cheung <659@holbertonschool.com>
2019-05-02 21:21:47 +0000

    Changed file name

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    leni = len(argv) - 1
+    j = 1
+    if leni == 0:
+            print("{} arguments.".format(leni))
+    else:
+            print("{} arguments:".format(leni))
+    while leni >= j:
+        print("{}: {:s}".format(j, argv[j]))
+        j = j + 1
