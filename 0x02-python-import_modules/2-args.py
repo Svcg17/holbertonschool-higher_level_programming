757b7ac57b2d21eb7f6a66c7f0ba872a488a9385
Sofia Cheung <659@holbertonschool.com>
2019-05-02 21:26:43 +0000

    Added elif for number of arguments to be 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,10 @@ if __name__ == "__main__":
     j = 1
     if leni == 0:
             print("{} arguments.".format(leni))
+    elif leni == 1:
+        print("{} argument:".format(leni))
     else:
-            print("{} arguments:".format(leni))
+        print("{} arguments:".format(leni))
     while leni >= j:
         print("{}: {:s}".format(j, argv[j]))
         j = j + 1
