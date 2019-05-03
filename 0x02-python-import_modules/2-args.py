091ae2a89393afb2cc713cac4072f0e74a145fa2
Sofia Cheung <659@holbertonschool.com>
2019-05-03 01:14:58 +0000

    got rid of j,changed to for loop

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     leni = len(argv) - 1
-    j = 1
     if leni == 0:
             print("{} arguments.".format(leni))
     elif leni == 1:
         print("{} argument:".format(leni))
     else:
         print("{} arguments:".format(leni))
-    while leni >= j:
-        print("{}: {:s}".format(j, argv[j]))
-        j = j + 1
+    for i in range(1, len(argv)):
+        print("{}: {:s}".format(i, argv[i]))
