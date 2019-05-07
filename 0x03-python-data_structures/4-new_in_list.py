3c807d330595818cafb2238ed5baca0a9d5b3dcc
Sofia Cheung <659@holbertonschool.com>
2019-05-07 21:18:01 +0000

    Added range in for loop to start from 0

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+        new = my_list.copy()
         if idx < 0:
             return my_list
         elif idx >= len(my_list):
             return my_list
         else:
-            new = my_list.copy()
-            for i in new:
+            for i in range(0, len(my_list)):
                 if i == idx:
                     new[i] = element
                     return new
