32c9a83103e2de40205bda4575a872eeb405df6e
Sofia Cheung <659@holbertonschool.com>
2019-05-07 21:39:32 +0000

    Added if condition if list exists, and fixed if condition

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
         new = ""
-        for i, j in enumerate(my_string):
-            if j != 'C' and 'c':
-                new += j
-        return new
+        if my_string:
+            for i, j in enumerate(my_string):
+                if (j != 'C') & (j != 'c'):
+                    new += j
+            return new
