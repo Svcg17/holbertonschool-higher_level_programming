50b6d60aec2c030c74e458076c20533c659d13b1
Sofia Cheung <659@holbertonschool.com>
2019-05-07 22:04:30 +0000

    Deleted range in loop

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
         new = ""
-        if my_string:
-            for i in range(0, len(my_string)):
-                if (my_string[i] != 'C') & (my_string[i] != 'c'):
-                    new += my_string[i]
-            return new
+        for i in my_string:
+            if (i != 'C') & (i != 'c'):
+                new += i
+        return new
