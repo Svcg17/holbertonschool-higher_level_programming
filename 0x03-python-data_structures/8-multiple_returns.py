d5f9a32df53ff1408510cdb64ceda941d851d9c9
Sofia Cheung <659@holbertonschool.com>
2019-05-08 00:44:27 +0000

    Added case for when sentence doesn't exist

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+        leni = len(sentence)
         if sentence:
-            leni = len(sentence)
             first = sentence[0:1]
+            toup = leni, first
+            return toup
+        else:
+            first = None
             toup = leni, first
             return toup
