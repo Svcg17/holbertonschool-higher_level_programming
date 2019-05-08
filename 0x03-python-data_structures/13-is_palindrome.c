e21f8bb8aa3c06cb742add9c4687c90f79f0a182
Sofia Cheung <659@holbertonschool.com>
2019-05-08 05:58:58 +0000

    Initial commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * reverse_listint - reverses a listint_t linked list.
+ * @head: pointer to a pointer to a list to be reversed.
+ * Return: a pointer to the first node of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *t, *t2;
+
+	if (*head == NULL)
+		return (NULL);
+	t = *head;
+	while (t->next != NULL)
+	{
+		t2 = t->next;
+		t->next = t2->next;
+		t2->next = *head;
+		*head = t2;
+	}
+	return (*head);
+}
+/**
+ * is_palindrome - function that checks if list is a palindrome
+ * @head: pointer to a pointer to the first node of the list
+ * Return: 1 if it's palindrome, 0 if not palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *s = *head;
+	listint_t *f = *head;
+	listint_t *current = *head;
+	listint_t *mid = NULL;
+
+	if (*head == NULL)
+		return (1);
+	while (f != NULL && f->next != NULL)
+	{
+		s = s->next;
+		f = f->next->next;
+		mid = reverse_listint(&s);
+		if (current->n == mid->n)
+			return (1);
+	}
+return (0);
+}
+
+
+
