199212b00c60ba18c4056cb00716507aa4baed7a
Sofia Cheung <659@holbertonschool.com>
2019-05-09 20:12:44 +0000

    Initialized new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+        new = []
         if my_list:
             new = my_list.copy()
             for i, elem in enumerate(new):
