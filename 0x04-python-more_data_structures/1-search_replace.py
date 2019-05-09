f95feea803a2e3005935db19c64df23b6f06d147
Sofia Cheung <659@holbertonschool.com>
2019-05-09 21:00:46 +0000

    Replace function with list comprehension

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-        new = []
-        if my_list:
-            new = my_list.copy()
-            for i, elem in enumerate(new):
-                if elem == search:
-                    new[i] = replace
-            return new
-        else:
-            return None
+        return [replace if search == i else i for i in my_list]
