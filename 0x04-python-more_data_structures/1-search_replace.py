d91c99c4ea74c1a86f0e2365e18ddbacbbcf323b
Sofia Cheung <659@holbertonschool.com>
2019-05-09 20:10:19 +0000

    Initial commit

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+        if my_list:
+            new = my_list.copy()
+            for i, elem in enumerate(new):
+                if elem == search:
+                    new[i] = replace
+            return new
