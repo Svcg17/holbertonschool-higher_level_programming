b6ee2c37ec2674f75487708ce7c8d32e53b28dae
Sofia Cheung <659@holbertonschool.com>
2019-05-11 06:35:07 +0000

    Changed fist if condition to an and

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
         rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         total = 0
-        if (roman_string) or (isinstance(roman_string, str)):
+        if (roman_string) and (isinstance(roman_string, str)):
             for i in range(len(roman_string)):
                 if i + 1 >= len(roman_string):
                     total += rom[roman_string[i]]
