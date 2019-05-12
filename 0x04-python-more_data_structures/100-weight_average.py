98d77644cf3fcfd8c0d1d39e845d10e735abf388
Sofia Cheung <659@holbertonschool.com>
2019-05-12 01:00:12 +0000

    Initial commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+        mult = 0
+        suma = 0
+        if my_list:
+            for x in my_list:
+                mult += x[0] * x[1]
+                suma += x[1]
+        return mult / suma
