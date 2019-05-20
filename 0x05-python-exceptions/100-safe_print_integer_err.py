85a0ff2dfc5f557567782161c553d4a35902c2fd
Sofia Cheung <659@holbertonschool.com>
2019-05-20 23:21:27 +0000

    Initial commit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}\n".format(value), end="")
+        return True
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return False
