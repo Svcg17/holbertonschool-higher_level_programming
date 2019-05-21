43dbf3af01eb99a539509eb629253c069e11b81b
Sofia Cheung <659@holbertonschool.com>
2019-05-21 02:35:15 +0000

    Changed print format

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
-    import sys
     try:
-        print("{:d}\n".format(value), end="")
+        print("{:d}".format(value))
         return True
     except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
