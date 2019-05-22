27e5187fa6458fc9649d6fe8d08b4a43324feb8e
Sofia Cheung <659@holbertonschool.com>
2019-05-22 02:47:03 +0000

    Added if conditions of positionss

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -10,10 +10,10 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if any(not instance(i, int) for i in value):
-            raise TypeError("size musy be an integer")
-        if any(num < 0 for i in value):
-            raise TypeError("size must be >= 0")
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
@@ -22,9 +22,15 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not instance(value, tuple) or len(value) != 2:
+        if type(value) != tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = position
+        if type(value[0]) != int or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[1]) != int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         a = self.__size * self.__size
