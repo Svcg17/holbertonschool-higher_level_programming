94d81dae2803ecba6dbe09bc25b14451d647f665
Sofia Cheung <659@holbertonschool.com>
2019-05-25 05:57:35 +0000

    Initial commit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+""" The 5-text_indentation module"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these characters: '.\
+            ', '?', ':'
+        Args:
+            text (str): Text to change
+
+        Returns:
+            the changed text
+
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    delims = ".?:"
+    nen = ""
+    for i in text:
+        nen += i
+        if i in delims:
+            print(nen.strip())
+            print()
+            nen = ""
+    print(nen.strip(), end="")
