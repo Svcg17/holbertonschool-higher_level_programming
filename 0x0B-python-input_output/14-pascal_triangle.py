83ac8aa346bf87adf7ce840958cbfb6ac1508a10
Sofia Cheung <659@holbertonschool.com>
2019-06-08 22:03:04 +0000

    Initial commit

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" The 14-pascal_triangle """
+
+
+def pascal_triangle(n):
+    """ returns a list of lists of integers representing the Pascal's
+    triangle of n
+
+    Args:
+        n(int): an integer to be used to represent the triangle
+    """
+    lista = []
+    if (n <= 0):
+        return lista
+
+    lista = [[1 for i in range(0, row + 1)] for row in range(0, n)]
+    for row in range(1, n):
+        for num in range(1, row):
+            lista[row][num] = lista[row - 1][num] + lista[row - 1][num - 1]
+    return lista
