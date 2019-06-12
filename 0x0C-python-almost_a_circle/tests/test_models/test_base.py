198204f9b052647a18f1b94685f8598abdc4bcc3
Sofia Cheung <659@holbertonschool.com>
2019-06-12 05:16:41 +0000

    Initial commit test files

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,53 @@
+"""Unittest for Base class"""
+
+
+import unittest
+from models.base import Base
+
+class Test_base(unittest.TestCase):
+    """ A Test_base class"""
+    def test_type(self):
+        """ Testing for type of instance"""
+        base1 = Base()
+        self.assertTrue(type(base1) == Base)
+
+    def test_withparam(self):
+        """Testing creating instance with parameter"""
+        base3 = Base(12)
+        self.assertTrue(type(base3) == Base)
+
+    def test_difffloats(self):
+        """Testing for floats, special numbers"""
+        b1 = Base(12.4)
+        self.assertEqual(b1.id, 12.4)
+        b2 = Base(float('nan'))
+        self.assertTrue(b2)
+        self.assertEqual(b2 is float('nan'), False)
+        b3 = Base(float('inf'))
+        self.assertTrue(b3)
+        self.assertEqual(b2 is float('nan'), False)
+
+    def test_noneid(self):
+        """Testing for a None id"""
+        bb = Base(None)
+        self.assertEqual(bb.id, 4)
+
+    def test_unknowd(self):
+        """Testing for unknown type"""
+        with self.assertRaises(NameError):
+            bbb = Base(sfo)
+
+    def test_checkid(self):
+        """Testing id of instance base"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,219 @@
+"""Unittest for the Rectangle class"""
+
+
+import unittest
+import sys
+from io import StringIO
+from models.base import Base
+from models.rectangle import Rectangle
+
+class Test_rectangle(unittest.TestCase):
+    """A Test_rectangle class
+    """
+    def resetnb(self):
+        """Resets __nb_objects"""
+        Base._Base__nb_objects = 0
+
+
+    """A Test_rectangle class"""
+    def test_assignment(self):
+        """Creating instance with values"""
+        r1 = Rectangle(10, 2)
+
+    def test_oneparam(self):
+        """Passing one arg to instance declaration"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(3)
+
+    def test_allparams(self):
+        """Passing all valid parameters"""
+        r1 = Rectangle(1, 2, 3, 4, 5)
+
+    def test_str(self):
+        """Passing str to the instance"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle("sdf")
+
+    def test_noparam(self):
+        """Passing no arguments"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle()
+
+    def test_unknowntype(self):
+        """Passing unknown parameter"""
+        with self.assertRaises(NameError):
+            r1 = Rectangle(soda)
+
+    def test_toomany(self):
+        """Passing more args than expected"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, 3, 4, 5, 6, 7)
+
+    def test_negatives(self):
+        """Passing only negative numbers"""
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-1, -3, -2, -2, -3)
+
+    def test_neganndpos(self):
+        """Passing both negative and positve integers"""
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(2, -3, 23, -2, -3)
+
+    def test_zero(self):
+        """Passing zeros
+            r1 = pass 0 to x or y
+            r2 = pass 0 to width or height
+        """
+        r1 = Rectangle(32, 3, 0, 4, 4)
+        with self.assertRaises(ValueError):
+            r2 = Rectangle(0, 8, 8, 7, 6)
+
+    def test_specialfloat(self):
+        """Passing different floats as param
+            r1 = pass float to x or y
+            r2 = pass float to width or heigth
+        """
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(23.4, 3, 4, 5, 4)
+            r2 = Rectangle(45, 6 , 5.4, 5.4, 5)
+            r3 = Rectangle(3, float('inf'), 3, 4)
+            r4 = Rectangle(3, 3, float('nan'), 4, 4)
+
+    def test_None(self):
+        """Passing none as arg"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(None, 67)
+
+    def test_area(self):
+        """Testing area method"""
+        r1 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r1.area(), 56)
+
+    def test_areawithparam(self):
+        """Test params being passed to area function"""
+        r1 = Rectangle(8, 7, 0, 0, 12)
+        with self.assertRaises(TypeError):
+            r1.area(3)
+
+    def test_display(self):
+        """Testing display method"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        r2 = Rectangle(2, 2)
+        r2.display()
+        sys.stdout = sys.__stdout__
+        assert outvar.getvalue() == '##\n##\n'
+
+    def test_display_x_y_(self):
+        """Testing display method with x and y variables"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        r2 = Rectangle(2, 3, 2, 2)
+        r2.display()
+        sys.stdout = sys.__stdout__
+        assert outvar.getvalue() == '\n\n  ##\n  ##\n  ##\n'
+ 
+    def test_sttrr(self):
+        """Testing the __str__ method"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        r3 = Rectangle(4, 6, 2, 1, 12)
+        print(r3)
+        sys.stdout = sys.__stdout__
+        assert outvar.getvalue() == "[Rectangle] (12) 2/1 - 4/6\n"
+
+    def test_strnameerror(self):
+        """trying to print unknown value"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        with self.assertRaises(NameError):
+            r3 = Rectangle(4, 6, 2, 1, 12)
+            print(sf)
+        sys.stdout = sys.__stdout__
+
+    def test_update(self):
+        """Testing update method"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        r3 = Rectangle(10, 10, 10, 10)
+        r3.update(89, 2, 3, 4, 5)
+        print(r3)
+        sys.stdout = sys.__stdout__
+
+    def test_updatemore(self):
+        """Testing too many params in update"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        r3 = Rectangle(10, 10, 10, 10, 10)
+        r3.update(89, 2, 3, 4, 5, 6)
+        print(r3)
+        sys.stdout = sys.__stdout__
+
+    def test_updatenonint(self):
+        """Testing for ints in the update methods"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        with self.assertRaises(TypeError):
+            r3 = Rectangle(10, 10, 10, 10, 10)
+            r3.update(89, 2, "Df")
+            r3.update(34.4, 4, 4)
+        sys.stdout = sys.__stdout__
+
+    def test_updatenoparam(self):
+        """Testing for no params """
+        outvar = StringIO()
+        sys.stdout = outvar
+        r3 = Rectangle(10, 10, 10, 10, 10)
+        r3.update()
+        print(r3)
+        sys.stdout = sys.__stdout__
+
+    def test_updatenegative(self):
+        """Testing for neg params """
+        outvar = StringIO()
+        sys.stdout = outvar
+        with self.assertRaises(ValueError):
+            r3 = Rectangle(10, 10, 10, 10, 10)
+            r3.update(-34, -4, 4)
+        sys.stdout = sys.__stdout__
+
+    def test_updatekwargs(self):
+        """Testing update with *kwargs"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        r3 = Rectangle(10, 10, 10, 10, 10)
+        r3.update(height=1)
+        print(r3)
+        r3.update(x=3, y=4)
+        print(r3)
+        r3.update(x=1, height=2, y=3, width=4)
+        print(r3)
+        sys.stdout = sys.__stdout__
+
+    def test_morekwargs(self):
+        """Testing for unknown arggs:"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        r3 = Rectangle(10, 10, 10, 10, 10)
+        r3.update(height=1)
+        print(r3)
+        r3.update(x=3, y=4)
+        print(r3)
+        r3.update(x=1, height=2, y=3, width=4, ji=444)
+        print(r3)
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "[Rectangle] (1) 1/3 - 4/2\n"
+
+    def test_updatekwargs(self):
+        """Testing update with *kwargs"""
+        outvar = StringIO()
+        sys.stdout = outvar
+        with self.assertRaises(SyntaxError):
+            r3 = Rectangle(10, 10, 10, 10, 10)
+            r3.update(height=)
+            print(r3)
+        sys.stdout = sys.__stdout__
+
+
+if __name__ == "__main__":
+    unittest.main()
