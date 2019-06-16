bd3b6b136d58190361e437932e19c67f214fa9ce
Sofia Cheung <659@holbertonschool.com>
2019-06-16 06:00:21 +0000

    First commit

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * swap_ints - swaps two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp = 0;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sorting algorithm.
+ *
+ * @array: array of integers to be sorted
+ * @size: size of the array of integers
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (j = 1; j < size; j++)
+	{
+		for (i = 1; i < size; i++)
+		{
+			if (array[i] < array[i - 1])
+			{
+				swap_ints(&array[i - 1], &array[i]);
+				print_array(array, size);
+			}
+		}
+	}
+}
