5805f967584774db0969e3925c92f46a631f87f0
Sofia Cheung <659@holbertonschool.com>
2019-06-18 04:33:07 +0000

    Added comments, fixed betty errors

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -14,7 +14,15 @@ void swap_ints(int *a, int *b)
 	*b = temp;
 }
 
-
+/**
+ * partition - gets pivot
+ * places pivot in place in the array.
+ * @array: array of integers to be sorted.
+ * @low: minimun number in array
+ * @high: maximum number in array
+ * @size: size of the array of integers
+ * Return: index value of pivot
+ */
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
@@ -39,15 +47,26 @@ int partition(int *array, int low, int high, size_t size)
 
 	return (i + 1);
 }
-
+/**
+ * recursion - recursively calls itself to call partition function
+ * takes care of passing to partition for sorting the left and
+ * right part of the rest of the numbers
+ * @array: array of integers to be sorted.
+ * @low: minimum index value of array.
+ * @high: maximum index value of array.
+ * @size: size of the array.
+ */
 void recursion(int *array, int low, int high, size_t size)
 {
 	int pi;
 
-	if(low < high)
+	if (low < high)
 	{
+		/*store pivot index in pi*/
 		pi = partition(array, low, high, size);
+		/*recursively call function from 0 to pivot*/
 		recursion(array, low, pi - 1, size);
+		/*recursively call function from pivot to max idx*/
 		recursion(array, pi + 1, high, size);
 	}
 }
@@ -62,5 +81,6 @@ void quick_sort(int *array, size_t size)
 {
 	if (array == NULL)
 		return;
+	/*call recursion func low = 0, high = size - 1*/
 	recursion(array, 0, size - 1, size);
 }
