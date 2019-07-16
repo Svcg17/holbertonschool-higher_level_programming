f56aef1cdec439bd1c0eade574d09ee04b5e21d7
Sofia Cheung <659@holbertonschool.com>
2019-07-16 19:49:09 +0000

    Fixed syntax for creating user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
 -- user_0d_1 should have all privileges on your MySQL server, the user_0d_1 password should be set to user_0d_1_pwd.
-CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-
