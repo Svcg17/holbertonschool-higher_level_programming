7e6a1785d418ae1028fc1956f4e980e6167fd3f0
Svcg17 <sofiavictoria17@gmail.com>
2019-08-11 01:17:11 +0000

    Changing host to localhost

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,7 @@
 if __name__ == '__main__':
     import MySQLdb
     import sys
-    db = MySQLdb.connect(host="127.0.0.1", port=3306,
+    db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute('SELECT * FROM states WHERE name LIKE \'N%\' ORDER BY id')
