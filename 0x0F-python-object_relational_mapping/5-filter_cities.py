90e29df9209873e76b8dd9f36d36b9d10cb89562
Svcg17 <sofiavictoria17@gmail.com>
2019-08-11 03:08:29 +0000

    Added condition to print a newline if condition is not met

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,15 +7,18 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    v = '''SELECT cities.name FROM states
-           INNER JOIN cities ON states.id = cities.state_id
+    v = '''SELECT cities.name FROM cities
+           JOIN states ON states.id = cities.state_id
            WHERE states.name=%s ORDER BY cities.id'''
     cur.execute(v, (sys.argv[4],))
     rows = cur.fetchall()
-    for i in range(0, len(rows)):
-        if i != len(rows) - 1:
-            print("{}, ".format(rows[i][0]), end="")
-        else:
-            print("{}".format(rows[i][0]))
+    if len(rows):
+        for i in range(0, len(rows)):
+            if i != len(rows) - 1:
+                print("{}, ".format(rows[i][0]), end="")
+            else:
+                print("{}".format(rows[i][0]))
+    else:
+        print('')
     cur.close()
     db.close()
