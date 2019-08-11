0f27d94d455cebadff7aebfd7e645e5a78edf96d
Svcg17 <sofiavictoria17@gmail.com>
2019-08-11 03:32:34 +0000

    Task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-from model_state import Base, State
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-from sqlalchemy import create_engine
-
-
+"""
+lists all State objects that contain the letter
+a from the database hbtn_0e_6_usa
+"""
 if __name__ == '__main__':
-	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    from model_state import Base, State
+    import sqlalchemy
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from sqlalchemy import create_engine
 
-	Session = sessionmaker(bind=engine)
-	Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
 
-	session = Session()
-	for state in session.query(State).order_by(State.id).all():
-		if 'a' in state.name:
-			print("{}: {}".format(state.id, state.name))
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
-	session.close()
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
+    session.close()
