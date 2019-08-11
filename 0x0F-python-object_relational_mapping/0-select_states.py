a0f74a1eee6bd0e3ed63ebee007a5682c47c6eee
Svcg17 <sofiavictoria17@gmail.com>
2019-08-11 01:13:43 +0000

    Adding id order

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,10 @@
 if __name__ == '__main__':
     import MySQLdb
     from sys import argv
-    db = MySQLdb.connect(host=localhost,
+    db = MySQLdb.connect(host='localhost',
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute('SELECT * FROM states')
+    cur.execute('SELECT * FROM states ORDER BY id')
     rows = cur.fetchall()
     for row in rows:
         print(row)
