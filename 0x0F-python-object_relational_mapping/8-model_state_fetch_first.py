986fa3aabc28ecbd07cfa12b1663bb411780fc15
Svcg17 <sofiavictoria17@gmail.com>
2019-08-11 03:25:49 +0000

    Fixed typo

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """script that prints the first State object from the database
 """
-from model_state import Base, State
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-from sqlalchemy import create_engine
-
 if __name__ == '__main__':
+    from model_state import Base, State
+    import sqlalchemy
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from sqlalchemy import create_engine
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                            argv[2], argv[3]), pool_pre_ping=True)
 
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     session = Session()
     firstt = session.query(State).order_by(State.id).first()
-    if first:
+    if firstt:
         print("{}: {}".format(firstt.id, firstt.name))
     else:
         print("Nothing")
