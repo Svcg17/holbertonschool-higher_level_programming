ec518ef8d4ae9dd2274c450132bbbe134a2cca35
Svcg17 <sofiavictoria17@gmail.com>
2019-08-26 19:49:14 +0000

    Added \t instead of spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
         html = r.read()
         print('Body response:')
-        print('    - type: {}'.format(type(html)))
-        print('    - content: {}'.format(html))
-        print('    - utf8 content: OK')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: OK')
