80f4e0a4f8051c6d8e3bb09ef69bb539494fe1cc
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-18 00:31:08 +0000

    Changing == to ===

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,13 @@
 if (process.argv.length === 2 || process.argv.length === 3) {
   console.log('0');
 } else {
-	let second = 0;
-	let i = 0;
-	const array = process.argv.slice(2).sort()
-	max = Math.max.apply(Math, array)
-	for (i in array) {
-		if (array[i] == max) {
-			i--
-			console.log(array[i])
-		}
-	}
+  let i = 0;
+  const array = process.argv.slice(2).sort();
+  const max = Math.max.apply(Math, array);
+  for (i in array) {
+    if (array[i] === String(max)) {
+      i--;
+      console.log(array[i]);
+    }
+  }
 }
