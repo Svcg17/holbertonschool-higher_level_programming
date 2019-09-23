fbd484a4116f4ba8c084f7db21a3f2566ecf5631
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-23 22:48:17 +0000

    Using let instead of var

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+
 exports.nbOccurences = function (list, searchElement) {
-  var counter = 0;
+  let counter = 0;
   for (let i = 0; list[i]; i++) {
     if (list[i] === searchElement) {
       counter++;
