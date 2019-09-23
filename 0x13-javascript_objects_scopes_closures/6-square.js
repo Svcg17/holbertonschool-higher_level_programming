396f57417eb0922fd28724b9d88da60f54a3b11f
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-23 22:04:03 +0000

    Adding charPrint instance method to class square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.size; i++) {
+        console.log(c.repeat(this.size));
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+module.exports = Square;
