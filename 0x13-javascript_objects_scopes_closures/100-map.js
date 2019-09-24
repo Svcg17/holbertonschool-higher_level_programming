f5c60eddd6897f7701bfb7ea7f3c91621e2f4853
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-24 01:04:42 +0000

    Fixing comments

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-const newlist = [];
+// const newlist = [];
 
-list.map((num, index) => {
+const newlist = list.map((num, index) => {
   // newlist.push(num * index);
   return (num * index);
 });
