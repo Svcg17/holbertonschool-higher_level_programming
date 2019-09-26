43653e44d3d8d6f6df633294bf3591891756d5d3
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-26 23:10:37 +0000

    Using toggleClass instead of if conditions

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,9 +1,3 @@
 $('#toggle_header').click(function () {
-  if ($('header').hasClass('green')) {
-    $('header').removeClass('green');
-    $('header').addClass('red');
-  } else if ($('header').hasClass('red')) {
-    $('header').removeClass('red');
-    $('header').addClass('green');
-  }
+  $('header').toggleClass('red green');
 });
